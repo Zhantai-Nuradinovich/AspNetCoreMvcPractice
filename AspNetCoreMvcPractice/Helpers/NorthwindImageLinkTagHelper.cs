aee5f5faf141bdcba32676a089b210b3bc10e625
Zhantai <jantai.nuradinovich@gmail.com>
2022-08-15 12:37:17 +0600

    Refactor and additional visual elements to _SecondLayout

--- AspNetCoreMvcPractice/Helpers/NorthwindImageLinkTagHelper.cs
+++ AspNetCoreMvcPractice/Helpers/NorthwindImageLinkTagHelper.cs
@@ -8,13 +8,13 @@ namespace AspNetCoreMvcPractice.Helpers
 {
     public class NorthwindImageLinkTagHelper : TagHelper
     {
-        public int Id { get; set; }
+        public int ImageId { get; set; }
         public string Text { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
             output.TagMode = TagMode.StartTagAndEndTag;
-            output.Attributes.SetAttribute("href", "Images/" + Id);
+            output.Attributes.SetAttribute("href", "Images/" + ImageId);
             output.Content.SetContent(Text);
         }
     }
